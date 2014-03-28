deea8b94f07de8af32855ccbf16123a59e7fe72e
Wayne Durack <waynedurack@wd.local>
2014-03-28 14:34:33 +0000

    Setup

---
+++ src/lib/related-reading.js
@@ -0,0 +1,12 @@
+// related-reading.js
+
+"use strict"
+var request = require('superagent')
+    , queryRelatedReading
+
+queryRelatedReading = function (options, callback) {
+  
+}
+
+
+exports.query = queryRelatedReading
